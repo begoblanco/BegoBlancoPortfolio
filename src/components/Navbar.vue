d78be474868644f05aa0abdb7f314872ab979161
begoblanco <begoblanfer@gmail.com>
2024-11-22 16:24:22 +0100

    fix

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -23,7 +23,7 @@ export default {
 </script>
 
 <template>
-  <nav class=" dark:bg-pink-400 sticky top-0">
+  <nav class=" bg-pink-400 sticky top-0">
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
       <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
         <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">☆ My Portfolio ☆</span>
